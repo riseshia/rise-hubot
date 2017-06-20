abfe005e9a1a7d499a229a4bece2f557971082de
Shia <rise.shia@gmail.com>
2017-06-20 11:52:36 +0900

    Use attachment for readability

--- scripts/news.js
+++ scripts/news.js
@@ -7,10 +7,11 @@
 // Configuration:
 //   None
 
-const cheerio = require('cheerio')
+const $ = require('cheerio')
 
 const room = 'general'
 
+// Return history url 1 day ago
 const targetUrl = () => {
   const date = new Date()
   date.setDate(date.getDate() - 1) // need to get date manipulation support
@@ -20,39 +21,47 @@ const targetUrl = () => {
   return `http://news.erogame-tokuten.com/history/${year}-${month}-${day}`
 }
 
-const parseHtml = (htmlText) => {
-  const $ = cheerio.load(htmlText)
-  const nodes = $('.whats_new div:last-child')
+const parsePerNews = (_i, news) => {
+  const $news = $(news)
+  const textRemovedCite = $news.html().replace('<cite>', ' *').replace('</cite>', '* ')
+  const contents = $news.html(textRemovedCite).text()
+
+  return '- ' + contents
+}
+
+const parseHtmlToAttachments = (htmlText) => {
+  const $html = $(htmlText)
+  const nodes = $html.find('.whats_new div:last-child')
   if (nodes.length === 0) {
     return '機能は新しいニュースがありませんでした。'
   }
 
   return nodes.map((_i, el) => {
     const $el = $(el)
-    const companyName = $el.find('a').text()
-    const newsText = $el.find('li').map((_i, news) => {
-      const textRemovedCite = $(news).html().replace('<cite>', ' *').replace('</cite>', '* ')
-      const contents = $(news).html(textRemovedCite).text()
+    const newsText = $el.find('li').map(parsePerNews).get().join('\n')
 
-      return '- ' + contents
-    }).get().join('\n')
-    return `> ${companyName}\n${newsText}`
-  }).get().join('\n')
+    return {
+      title: $el.find('a').text(),
+      title_link: $el.find('a').attr('href'),
+      text: newsText
+    }
+  }).get()
 }
 
 module.exports = function (robot) {
   robot.router.get('/news', (_req, res) => {
-    console.log(targetUrl())
     robot.http(targetUrl()).get()((err, res, body) => {
       if (err || res.statusCode !== 200) {
-        robot.messageRoom(
-          room,
-          `ニュースの処理中にエラーが発生しました。\n${err}`
-        )
+        robot.messageRoom(room, `ニュースの処理中にエラーが発生しました。\n${err}`)
       } else {
-        robot.messageRoom(room, parseHtml(body))
+        const data = {
+          text: '昨日のニュースまとめです。',
+          attachments: parseHtmlToAttachments(body)
+        }
+        robot.messageRoom(room, data)
       }
     })
+
     res.send('OK')
   })
 }
